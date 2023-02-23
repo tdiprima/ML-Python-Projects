2f968d39c975fd6ed2b0dddfb31a29af10fdc65f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-02-23 14:45:05 -0500

    up

--- bear_training/gan-keras.py
+++ bear_training/gan-keras.py
@@ -1,7 +1,8 @@
-# Import necessary libraries
+"""
+Epoch: 0, Discriminator Loss: 0.6365200877189636, Generator Loss: 0.7656040191650391
+"""
 import numpy as np
 from keras.datasets import mnist
-# import tensorflow as tf
 from keras.layers import Dense, LeakyReLU, BatchNormalization, Input
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
@@ -12,14 +13,17 @@ generator.add(Dense(128, input_dim=100))
 generator.add(LeakyReLU(alpha=0.01))
 generator.add(BatchNormalization())
 generator.add(Dense(784, activation='tanh'))
-generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5))
+
+# todo: lr deprecated, use learning_rate
+# generator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5))
+generator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002, beta_1=0.5))
 
 # Define the discriminator network
 discriminator = Sequential()
 discriminator.add(Dense(128, input_dim=784))
 discriminator.add(LeakyReLU(alpha=0.01))
 discriminator.add(Dense(1, activation='sigmoid'))
-discriminator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5))
+discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002, beta_1=0.5))
 
 # Combine the generator and discriminator networks to form the GAN
 z = Input(shape=(100,))
@@ -27,12 +31,10 @@ img = generator(z)
 discriminator.trainable = False
 validity = discriminator(img)
 gan = Model(z, validity)
-gan.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5))
+gan.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002, beta_1=0.5))
 
 # Load the dataset
 (X_train, _), (_, _) = mnist.load_data()
-# Tensorflow-alert:
-# (X_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
 
 # Normalize and flatten the images
 X_train = X_train / 127.5 - 1.
@@ -40,7 +42,7 @@ X_train = np.reshape(X_train, (X_train.shape[0], 784))
 
 # Train the GAN
 # epochs = 10000 # todo: This is the one.
-epochs = 500
+epochs = 200
 batch_size = 128
 for epoch in range(epochs):
     # Generate random noise
@@ -61,3 +63,5 @@ for epoch in range(epochs):
     # Print the losses every 100 epochs
     if epoch % 100 == 0:
         print(f"Epoch: {epoch}, Discriminator Loss: {d_loss}, Generator Loss: {g_loss}")
+
+    # todo: final output??
