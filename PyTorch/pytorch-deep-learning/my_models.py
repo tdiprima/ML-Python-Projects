82eca24b0342841a55d7a49d5d2251702dadb259
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-03-15 13:50:07 -0400

    update model

--- PyTorch/pytorch-deep-learning/my_models.py
+++ PyTorch/pytorch-deep-learning/my_models.py
@@ -2,19 +2,33 @@ import torch
 from torch import nn
 
 
-# Define your model class
 class LinearRegressionModel(nn.Module):
+    """
+    Create a Linear Regression model class
+    Almost everything in PyTorch is a nn.Module (like neural network lego blocks)
+    Start with random weights and bias (this will get adjusted as the model learns)
+    requires_grad=True: Can we update this value with gradient descent?
+    PyTorch loves float32.
+    """
+
     def __init__(self):
-        # super().__init__()
+        super().__init__()
 
-        super(LinearRegressionModel, self).__init__()
-        # define your model layers here
-        self.weights = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+        self.weights = nn.Parameter(
+            torch.randn(1, dtype=torch.float),
+            requires_grad=True
+        )
 
-        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+        self.bias = nn.Parameter(
+            torch.randn(1, dtype=torch.float),
+            requires_grad=True
+        )
 
-    # Forward = computation
-    # "x" is the input data (e.g. training/testing features)
+    """
+    Forward defines the computation in the model
+    "x" is the input data (e.g. training/testing features)
+    This is the linear regression formula (y = m*x + b)
+    """
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # linear regression formula (y = m*x + b)
         return self.weights * x + self.bias
