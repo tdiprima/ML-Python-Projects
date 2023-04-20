5b5410e3415945a34338f7448593f196d997e9c3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-04-20 16:32:47 -0400

    image segmentation in tensorflow

---
+++ TensorFlow/image_segmentation/train_unet_tflow.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+https://stackoverflow.com/questions/50304156/tensorflow-allocation-memory-allocation-of-38535168-exceeds-10-of-system-memor
+https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information/42121886#42121886
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow_addons as tfa
+import PIL
+from tensorflow.keras.layers import Dense, Conv2D, Input, MaxPool2D, UpSampling2D, Concatenate, Conv2DTranspose
+from tensorflow.keras import Sequential,Model
+from tensorflow.keras.losses import BinaryCrossentropy
+import tensorflow as tf
+from tqdm.notebook import tqdm
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
+import os
+from tensorflow.keras.backend import flatten
+import tensorflow.keras.backend as K
+
+# Prepare data generator (no augmentation)
+data_dir = '/home/tdiprima/projects/image_segmentation/data/train_images/'
+mask_dir = '/home/tdiprima/projects/image_segmentation/data/train_masks/'
+
+all_images = os.listdir(data_dir)
+
+to_train = 1  # ratio of number of train set images to use
+total_train_images = all_images[:int(len(all_images) * to_train)]
+
+WIDTH = 512  # actual : 1918//1920 divisive by 64
+HEIGHT = 512  # actual : 1280
+# BATCH_SIZE = 5
+BATCH_SIZE = 2
+
+# split train set and test set
+train_images, validation_images = train_test_split(total_train_images, train_size=0.8, test_size=0.2, random_state=0)
+
+
+# generator that we will use to read the data from the directory
+def data_gen_small(data_dir, mask_dir, images, batch_size, dims):
+    """
+    data_dir: where the actual images are kept
+    mask_dir: where the actual masks are kept
+    images: the filenames of the images we want to generate batches from
+    batch_size: self-explanatory
+    dims: the dimensions in which we want to rescale our images, tuple
+    """
+    while True:
+        ix = np.random.choice(np.arange(len(images)), batch_size)
+        imgs = []
+        labels = []
+        for i in ix:
+            # images
+            original_img = load_img(data_dir + images[i])
+            resized_img = original_img.resize(dims)
+            array_img = img_to_array(resized_img) / 255
+            imgs.append(array_img)
+
+            # masks
+            original_mask = load_img(mask_dir + images[i].split(".")[0] + '_mask.gif')
+            resized_mask = original_mask.resize(dims)
+            array_mask = img_to_array(resized_mask) / 255
+            labels.append(array_mask[:, :, 0])
+
+        imgs = np.array(imgs)
+        labels = np.array(labels)
+        yield imgs, labels.reshape(-1, dims[0], dims[1], 1)
+
+
+# generator that we will use to read the data from the directory with random augmentation
+def data_gen_aug(data_dir, mask_dir, images, batch_size, dims):
+    """
+    data_dir: where the actual images are kept
+    mask_dir: where the actual masks are kept
+    images: the filenames of the images we want to generate batches from
+    batch_size: self-explanatory
+    dims: the dimensions in which we want to rescale our images, tuple
+    """
+    while True:
+        ix = np.random.choice(np.arange(len(images)), batch_size)
+        imgs = []
+        labels = []
+        for i in ix:
+            # read images and masks
+            original_img = load_img(data_dir + images[i])
+            original_mask = load_img(mask_dir + images[i].split(".")[0] + '_mask.gif')
+
+            # transform into ideal sizes
+            resized_img = original_img.resize(dims)
+            resized_mask = original_mask.resize(dims)
+
+            # add random augmentation > here we only flip horizontally
+            if np.random.random() < 0.5:
+                resized_img = resized_img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+                resized_mask = resized_mask.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+
+            array_img = img_to_array(resized_img) / 255
+            array_mask = img_to_array(resized_mask) / 255
+
+            imgs.append(array_img)
+            labels.append(array_mask[:, :, 0])
+
+        imgs = np.array(imgs)
+        labels = np.array(labels)
+        yield imgs, labels.reshape(-1, dims[0], dims[1], 1)
+
+
+# generator for train and validation data set
+train_gen = data_gen_aug(data_dir, mask_dir, train_images, BATCH_SIZE, (WIDTH, HEIGHT))
+val_gen = data_gen_small(data_dir, mask_dir, validation_images, BATCH_SIZE, (WIDTH, HEIGHT))
+
+
+# Set up Unet model
+
+# Define down and up layers that will be used in Unet model
+
+
+def down(input_layer, filters, pool=True):
+    conv1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(input_layer)
+    residual = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv1)
+    if pool:
+        max_pool = MaxPool2D()(residual)
+        return max_pool, residual
+    else:
+        return residual
+
+
+def up(input_layer, residual, filters):
+    filters = int(filters)
+    upsample = UpSampling2D()(input_layer)
+    upconv = Conv2D(filters, kernel_size=(2, 2), padding="same")(upsample)
+    concat = Concatenate(axis=3)([residual, upconv])
+    conv1 = Conv2D(filters, (3, 3), padding='same', activation='relu')(concat)
+    conv2 = Conv2D(filters, (3, 3), padding='same', activation='relu')(conv1)
+    return conv2
+
+
+# Make a custom U-net implementation.
+filters = 64
+input_layer = Input(shape=[WIDTH, HEIGHT, 3])
+layers = [input_layer]
+residuals = []
+
+# Down 1
+d1, res1 = down(input_layer, filters)
+residuals.append(res1)
+
+filters *= 2
+
+# Down 2
+d2, res2 = down(d1, filters)
+residuals.append(res2)
+
+filters *= 2
+
+# Down 3
+d3, res3 = down(d2, filters)
+residuals.append(res3)
+
+filters *= 2
+
+# Down 4
+d4, res4 = down(d3, filters)
+residuals.append(res4)
+
+filters *= 2
+
+# Down 5
+d5 = down(d4, filters, pool=False)
+
+# Up 1
+up1 = up(d5, residual=residuals[-1], filters=filters / 2)
+filters /= 2
+
+# Up 2
+up2 = up(up1, residual=residuals[-2], filters=filters / 2)
+
+filters /= 2
+
+# Up 3
+up3 = up(up2, residual=residuals[-3], filters=filters / 2)
+
+filters /= 2
+
+# Up 4
+up4 = up(up3, residual=residuals[-4], filters=filters / 2)
+
+out = Conv2D(filters=1, kernel_size=(1, 1), activation="sigmoid")(up4)
+
+model = Model(input_layer, out)
+
+print(model.summary())
+
+
+# Use Tensorflow to write a custom dice_coefficient metric,
+# which is an effective indicator of how much two sets overlap with each other.
+def dice_coef(y_true, y_pred):
+    smooth = 1.
+    y_true_f = flatten(y_true)
+    y_pred_f = flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+# Include the epoch in the file name (uses `str.format`)
+checkpoint_path = "/home/tdiprima/projects/image_segmentation/data/unet_cp_full_512batch5/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights every epoch
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+    save_freq='epoch')
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8,
+                                              restore_best_weights=False
+                                              )
+
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
+                                                 factor=0.2,
+                                                 patience=3,
+                                                 verbose=1,
+                                                 min_delta=1e-3, min_lr=1e-6
+                                                 )
+
+adam = tf.keras.optimizers.Adam(learning_rate=0.0001)
+
+# Train model
+import sys
+
+try:
+    # Load weights and continue training
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    print("\nLatest:\n", latest)
+
+    if latest is not None:
+        model.load_weights(latest)
+
+    model.compile(optimizer=adam, loss=BinaryCrossentropy(), metrics=['accuracy', dice_coef])
+
+    model.fit(train_gen, callbacks=[cp_callback, early_stop, reduce_lr],
+              steps_per_epoch=np.ceil(float(len(train_images)) / float(BATCH_SIZE)),
+              epochs=100,
+              validation_steps=np.ceil(float(len(validation_images)) / float(BATCH_SIZE)),
+              validation_data=val_gen)
+
+    # save the model to a file
+    model.save("saved_model.pb")
+
+except Exception as e:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    print("\nType", exc_type)
+    print("\nErr:", exc_obj)
+    print("\nLine:", exc_tb.tb_lineno)
+    sys.exit(1)
