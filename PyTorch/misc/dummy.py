8ef90b3381fae6ad35f969edc9ed3f450b7dd7a0
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-09 16:43:39 -0400

    up

--- PyTorch/misc/dummy.py
+++ PyTorch/misc/dummy.py
@@ -3,12 +3,18 @@ import torch.nn as nn
 
 
 class MyModel(nn.Module):
+    """
+    Linear, input shape 1.
+    """
     def __init__(self, input_shape=1, hidden_units=10, output_shape=10):
         super(MyModel, self).__init__()
         self.fc1 = nn.Linear(input_shape, hidden_units)
         self.fc2 = nn.Linear(hidden_units, output_shape)
 
     def forward(self, x):
+        """
+        Forward pass has a relu.
+        """
         x = self.fc1(x)
         x = nn.functional.relu(x)
         x = self.fc2(x)
@@ -28,11 +34,11 @@ print("\noutput_tensor shape:", output_tensor.shape)
 
 # ===============================
 
-import torch
-import torch.nn as nn
-
 
 class MyModel1(nn.Module):
+    """
+    Conv2d, Linear, input shape (1, 28, 28).
+    """
     def __init__(self, input_shape=(1, 28, 28), hidden_units=10, output_shape=10):
         super(MyModel1, self).__init__()
         self.conv1 = nn.Conv2d(input_shape[0], hidden_units, kernel_size=3)
@@ -40,8 +46,8 @@ class MyModel1(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = nn.functional.relu(x)
-        x = x.view(x.size(0), -1)
+        x = nn.functional.relu(x)  # relu
+        x = x.view(x.size(0), -1)  # preserve the batch dimension, flatten the others
         x = self.fc1(x)
         return x
 
@@ -49,10 +55,12 @@ class MyModel1(nn.Module):
 # create model
 model = MyModel1()
 
-# create dummy tensor with shape [1, 1, 28, 28]
+# input_shape=(1, 28, 28), dummy tensor shape [1, 1, 28, 28]
 input_tensor = torch.randn(1, 1, 28, 28)
 
 # pass through model and check output shape
 output_tensor = model(input_tensor)
 assert output_tensor.shape == (1, 10)
 print("\noutput_tensor shape:", output_tensor.shape)
+
+exit(0)
